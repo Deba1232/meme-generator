0c32258e97cd9e7ce268cc4f126a94e162adda00
DeV <debanjan.das90@gmail.com>
2023-05-16 21:58:17 +0530

    top and bottom text made

--- src/components/Meme/Meme.jsx
+++ src/components/Meme/Meme.jsx
@@ -4,38 +4,83 @@ import './Meme.css'
 import memesData from '../../memesData';
 
 function Meme(){
-    const memes = memesData.data.memes;
-    const [memeImage,setMemeImage] = useState("");
+
+    const [meme,setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg",
+    });
+
+    const [allMemeImages,setAllMemeImages] = useState(memesData);
+
+    function handleChange(event){
+        const {name, value} = event.target;
+
+        setMeme(prevText => {
+            return(
+                {
+                    ...prevText,
+                    [name]: value,
+                }
+            );
+        })
+    }
 
     function getMemeImage(){
 
-        let memeImageSource = memes[Math.floor(Math.random()*memes.length)].url;
-        setMemeImage(memeImageSource);
+        const memesArray = allMemeImages.data.memes;
+        let randomImageUrl = memesArray[Math.floor(Math.random()*memesArray.length)].url;
+
+        setMeme(prevMemeData => {
+            return(
+                {
+                    ...prevMemeData,
+                    randomImage: randomImageUrl,
+                }
+            )
+        });
 
     }
 
     return (
-        <div className='meme'>
+        <main>
 
             <div className='form'>
                 
                 <section className='text-fields'>
-                    <input type='text' id='top-text' name='top-text' 
-                    placeholder='Top Text'/>
-                    <input type='text'  id='bottom-text' name='bottom-text' 
-                    placeholder='Bottom Text'/>
+                    <input 
+                        type='text' 
+                        id='top-text' 
+                        name="topText"
+                        placeholder="Top Text"
+                        value={meme.topText}
+                        onChange={handleChange}
+                    />
+
+                    <input 
+                        type='text'  
+                        id='bottom-text' 
+                        name="bottomText" 
+                        placeholder='Bottom Text'
+                        value={meme.bottomText}
+                        onChange={handleChange}
+                    />
+                    
                 </section>
 
-                <button onClick={getMemeImage}>Get a new meme image 
-                <i className="fa fa-picture-o" aria-hidden="true"></i></button>
+                <button onClick={getMemeImage}>
+                    Get a new meme image <i className="fa fa-picture-o" aria-hidden="true"></i>
+                </button>
                 
             </div>
 
-            <div className="image--container">
-                <img src={memeImage} alt="" className='meme--image' />
+            <div className="meme">
+                <img src={meme.randomImage} alt="" className='meme--image' />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
 
-        </div>
+        </main>
     );
 }
 
